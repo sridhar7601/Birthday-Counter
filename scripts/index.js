0f284af2ea394cf976b062f9df3ec931cc6f9738
sridhar <sridzar1234@gmail.com>
2024-04-18 15:18:23 +0530

    Date update

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Apr 18, 2024',
+  birthdate: 'Apr 19, 2024',
   name: 'Test-1'
 };
 
